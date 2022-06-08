3b0652306d28dee2bf94be67bd05b0c8bab560dd
King-Surge <kingsurge2022@gmail.com>
2022-06-08 13:43:13 -0500

    Create main.java

---
+++ Print Statementd/main.java
@@ -0,0 +1,13 @@
+class Print
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//prints statement in a new line
+		System.out.println("Ping!!");
+		System.out.println("Pong!!");
+		
+		// Prints everything in the same line
+		System.out.print("how are you");
+		System.out.print("Im Doing Fine");
+	}
+}
