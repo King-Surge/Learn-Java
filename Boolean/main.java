716dcf0c9b9d31b3a0482a5dadfc6be9e6f13ebf
King-Surge <kingsurge2022@gmail.com>
2022-06-08 14:24:01 -0500

    Create main.java

---
+++ Boolean/main.java
@@ -0,0 +1,16 @@
+class bool {
+
+	public static void main(String args[])
+	{
+
+		boolean a = false;
+		boolean b = true;
+
+		if (b == true){
+			System.out.println("Hello!!");
+		}
+		if(a == false){
+			System.out.println("Goodbye!!");		
+		}
+	}
+}
