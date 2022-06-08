135ea1fb632f2c6dc285dc1b2fc53461d17ce53b
King-Surge <kingsurge2022@gmail.com>
2022-06-08 15:08:57 -0500

    Update main.java

--- Type Casting/main.java
+++ Type Casting/main.java
@@ -1,1 +1,19 @@
 
+public class Main {
+  public static void main(String[] args) {
+    
+    // Narrowing Casting
+    double surge = 9.78;
+    int map = (int) surge; // Manual casting: double to int
+
+    System.out.println(surge);   // Outputs 9.78
+    System.out.println(map);      // Outputs 9
+    
+    // Widening Casting
+    int surging = 9;
+    double surger = surging; // Automatic casting: int to double
+
+    System.out.println(surging);      // Outputs 9
+    System.out.println(surger);   // Outputs 9.0
+  }
+}
