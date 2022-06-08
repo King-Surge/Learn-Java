23b2e831665e857eedabf96047bac4f7bc97a11a
King-Surge <kingsurge2022@gmail.com>
2022-06-08 14:29:12 -0500

    Create main.java

---
+++ data types/float/main.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+
+class float {
+
+	public static void main(String[] args)
+	{
+		// declaring a float
+		float value2 = 19.9857;
+    
+    // print the value of the float
+		System.out.println(value2);
+	}
+}
