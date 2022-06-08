9f4eb5baba38bcf7a3239ba30cfe25bdae96709b
King-Surge <kingsurge2022@gmail.com>
2022-06-08 14:55:39 -0500

    Create main.java

---
+++ data types/Strings/main.java
@@ -0,0 +1,15 @@
+class Strings {
+
+	public static void main(String args[])
+	{
+    // Declare String without using new operator 
+    String surge = "Hello World"; 
+    
+    // Declare String using new operator 
+    String surgea = new String("How Are You"); 
+    
+    System.out.println(surge);
+    System.out.println(surgea);
+    
+	}
+}
